cb2bdf730e8c6c80ae3038549832c1bc9505da2f
kevinvu184 <kevinvu184@gmail.com>
2022-03-21 01:04:38 +1100

    add second mechanism to prevent error

--- backend/lambda/listDealers.ts
+++ backend/lambda/listDealers.ts
@@ -1,19 +1,69 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { DynamoDBClient, UpdateItemCommand, UpdateItemCommandInput, GetItemCommand, GetItemCommandInput } from "@aws-sdk/client-dynamodb";
 import axios from 'axios';
+
+const client = new DynamoDBClient({ region: process.env.REGION });
+const CACHE_TABLE = process.env.CACHE_TABLE;
 
 export const handler: APIGatewayProxyHandler = async () => {
   const retries = 3
   const url = 'https://bb61co4l22.execute-api.us-west-2.amazonaws.com/development/dealers';
 
+  // first mechanism - retry 3 times
   for (let i = 0; i < retries; i++) {
     try {
-      const response = await axios.get(url);
+      const { data } = await axios.get(url);
+
+      // second mechanism - Update the cache response
+      const updateParams: UpdateItemCommandInput = {
+        TableName: CACHE_TABLE,
+        Key: {
+          PK: { S: 'DEALERS#' },
+          SK: { S: 'DEALERS#' }
+        },
+        UpdateExpression: 'SET #response = :response',
+        ExpressionAttributeValues: {
+          ':response': {
+            S: JSON.stringify(data)
+          }
+        },
+        ExpressionAttributeNames: {
+          '#response': 'response'
+        }
+      };
+      const updateCommand = new UpdateItemCommand(updateParams);
+      await client.send(updateCommand);
+
       return {
         statusCode: 200,
-        body: JSON.stringify(response.data),
+        body: JSON.stringify(data),
       };
     } catch (error) {
       console.error(error);
     }
+  }
+
+  // second mechanism - Get the cache response
+  // this segment is only executed if the first mechanism fails
+  const getParams: GetItemCommandInput = {
+    TableName: CACHE_TABLE,
+    Key: {
+      PK: { S: 'DEALERS#' },
+      SK: { S: 'DEALERS#' }
+    }
+  };
+  const getCommand = new GetItemCommand(getParams);
+
+  try {
+    const response = await client.send(getCommand);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(JSON.parse(response.Item.response.S))
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error)
+    };
   }
 };
