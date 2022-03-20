4593b80e22e2a88217184a8a2e3c05706e6ab6b2
kevinvu184 <kevinvu184@gmail.com>
2022-03-20 23:40:01 +1100

    rm outer try catch

--- backend/lambda/listVehicles.ts
+++ backend/lambda/listVehicles.ts
@@ -5,22 +5,15 @@ export const handler: APIGatewayProxyHandler = async (event) => {
   const retries = 3
   const url = `https://bb61co4l22.execute-api.us-west-2.amazonaws.com/development/vehicles/${event.pathParameters.bac}`;
 
-  try {
-    for (let i = 0; i < retries; i++) {
-      try {
-        const response = await axios.get(url);
-        return {
-          statusCode: 200,
-          body: JSON.stringify(response.data),
-        };
-      } catch (error) {
-        console.error(error);
-      }
+  for (let i = 0; i < retries; i++) {
+    try {
+      const response = await axios.get(url);
+      return {
+        statusCode: 200,
+        body: JSON.stringify(response.data),
+      };
+    } catch (error) {
+      console.error(error);
     }
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify(error),
-    };
   }
 };
